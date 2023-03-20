05e23fb022eb5160c67b2347ee7857c0b27a13f9
kundan <goswamikundan123@gmail.com>
2023-03-20 17:16:24 +0530

    Adding file which reads and write the contacts in a file.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the master branch of Address Book File i/o Problem");
+            //Console.WriteLine("Welcome to the master branch of Address Book File i/o Problem");
+
+            AddressBook myAddressBook = new AddressBook();
+            Contacts newContact1 = new Contacts("Kundan", "Kumar", "Inadia", "7975461361", "kundan.kr@example.com");
+            Contacts newContact2 = new Contacts("Thor", "Odinson", "USA", "1234567890", "thor.odinson@example.com");
+            
+
+            myAddressBook.AddContact(newContact1);
+            myAddressBook.AddContact(newContact2);
+
+            myAddressBook.PrintContacts();
+
+
+            string path = "C:\\Users\\kundan\\Desktop\\Bridgelab\\rfp257\\AddressBook3\\AddressBook3\\Newfile.txt";
+
+
+            myAddressBook.WriteToFile(path);
+            myAddressBook.ReadFromFile(path);
         }
     }
 }
